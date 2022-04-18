a6b014b1f285918b99a2b7b0eb1eae1151d3ac00
Diego Guerrero <zorro8815@gmail.com>
2022-04-18 15:21:06 -0400

    GET para obtener clima por ciudad

--- src/app/service/tiempo.service.ts
+++ src/app/service/tiempo.service.ts
@@ -7,4 +7,9 @@ import { Injectable } from '@angular/core';
 export class TiempoService {
 
   constructor(private http:HttpClient) { }
+
+  obtenertiempo(ciudad:string){
+
+return this.http.get(`http://api.weatherapi.com/v1/forecast.json?key=bd8522d5414e4784946150348222903&q=${ciudad}&days=3`)
+  }
 }
